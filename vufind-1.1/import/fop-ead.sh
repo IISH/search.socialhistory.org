7424572bd55e491747197204bb5db7602137bdf7
Lucien van Wouw <lwo@iisg.nl>
2011-12-12 14:19:33 +0100

    Sitemap robots.txt added

--- vufind-1.1/import/fop-ead.sh
+++ vufind-1.1/import/fop-ead.sh
@@ -3,7 +3,7 @@
 # Load all of the .jar files in the lib directory into the classpath
 folder=$VUFIND_HOME/caching/ead.xml/10622
 target=$VUFIND_HOME/caching/ead.pdf/10622
-for file in ${folder}/*.xml ; do
+for file in ${folder}/ARCH* ; do
     pdf=$(basename $file .xml).pdf
     fop -xml $file -xsl $VUFIND_HOME/import/xsl/ead_complete_fo.xsl -pdf $target/$pdf -param path $VUFIND_HOME/import/xsl
 done
