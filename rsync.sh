0d655634173c8408f7ff548c58f84aedbe466152
Lucien van Wouw <lwo@iisg.nl>
2012-02-13 15:30:38 +0100

    URLs in rsync were wrong

--- rsync.sh
+++ rsync.sh
@@ -3,7 +3,13 @@
 # Solr support replication. But the download time from the be0 and be1 can take rather long time and thus fail.
 # This script is therefor a workaround. It is run from the store0 node
 
-index0=/data/search.socialhistory.org.index0/vufind-1.1/solr/
+# If no parameter was given, we replicate the master index
+if [ "$1" == "" ];
+then
+	index0=/data/search.socialhistory.org.index0/vufind-1.1/solr/
+else
+	index0=$1
+fi
 
 
 for i in {0..1}
@@ -11,7 +17,9 @@ for i in {0..1}
 		be=/data/search.socialhistory.org.be"$i"/vufind-1.1/solr/
 		rsync --delete -avv $index0 $be
 		chown -R tomcat6:tomcat6 $be
-		wget -O /tmp/reload.txt http://erebus.be"$i".iisg.net:8080/solr/admin/multicore?action="RELOAD&core=biblio"
-		wget -O /tmp/reload.txt http://erebus.be"$i".iisg.net:8080/solr/admin/multicore?action="RELOAD&core=authority"
+		url=http://erebus.be"$i".iisg.net:8080/solr/admin/multicore?action="RELOAD&core=biblio"
+		wget -O /tmp/reload.txt "$url"
+		url=http://erebus.be"$i".iisg.net:8080/solr/admin/multicore?action="RELOAD&core=authority"
+		wget -O /tmp/reload.txt "$url"
 done
 
