7fa65a3b981229ac5c32a42facebac6efe42dca3
Lucien van Wouw <lwo@iisg.nl>
2013-02-06 22:15:27 +0100

    Add deleted xslt 2

--- vufind-1.1/import/catalog_normalizations/src/main/java/org/socialhistoryservices/solr/importer/Collate.java
+++ vufind-1.1/import/catalog_normalizations/src/main/java/org/socialhistoryservices/solr/importer/Collate.java
@@ -76,7 +76,7 @@ public class Collate {
             if (xsr.getEventType() == XMLStreamReader.START_ELEMENT) {
                 if (xsr.getLocalName().equals("record")) {
                     transformer.transform(new StAXSource(xsr), new StreamResult(writer));
-                    writer.write("\r\n");
+                    writer.write("\n");
                     counter++;
                 } else {
                     xsr.next();
