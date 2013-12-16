9332df09b64f7aa54ad67043635558248eb163f7
Lucien van Wouw <lwo@iisg.nl>
2013-12-16 16:05:32 +0100

    Sync to tmp folder first. Then move the tmp folder to replace the index.

--- vufind-1.1/custom/rsync.sh
+++ vufind-1.1/custom/rsync.sh
@@ -11,16 +11,17 @@ else
 	index0=$1
 fi
 
-
 for i in {0..3}
 	do
 		base=/data/search.socialhistory.org.be"$i"
 		be=$base/vufind-1.1/solr/
-		mkdir -p $be
-		rsync --delete -avv $index0 $be
+		tmp=$base/vufind-1.1/tmp/
+		if [ -d $tmp ] ; then
+		    rm -r $tmp
+		fi
+		mkdir -p $tmp
+		rsync --delete -avv $index0 $tmp
+		rm -r $be
+		mv $tmp $be
 		chown -R tomcat6:tomcat6 $base
-		#url=http://erebus.be"$i".iisg.net:8080/solr/admin/multicore?action="RELOAD&core=biblio"
-		#wget -O /tmp/reload.txt "$url"
-		#url=http://erebus.be"$i".iisg.net:8080/solr/admin/multicore?action="RELOAD&core=authority"
-		#wget -O /tmp/reload.txt "$url"
 done
