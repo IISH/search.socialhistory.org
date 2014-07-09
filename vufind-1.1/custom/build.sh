dab9dfd28d4558889874cff6674a02fb3ea9580b
Lucien van Wouw <lwo@iisg.nl>
2014-07-09 11:39:38 +0200

    Add windows exclusions to ignorefile

---
+++ vufind-1.1/custom/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Build
+#
+# Creates a tar file
+
+instance=$1
+if [ -z "$instance" ] ; then
+    echo "Need the instance name. E.g. 'search'"
+    exit -1
+fi
+
+version=$2
+if [ -z "$version" ] ; then
+    echo "Need the instance name. E.g. '1.0'"
+    exit -1
+fi
+
+mkdir target
+archive="target/${instance}-${bamboo_tag_version}.tar.gz"
+rm $archive
+tar -zcaf $archive vufind-1.1
+
+if [ -f $archive ] ; then
+    exit 0
+else
+    echo "Failed to build a package: $archive"
+    exit -1
+fi
+
+
