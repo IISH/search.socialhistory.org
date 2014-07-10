fe6571c9fd66f690de75a9c6368c19e88e35146e
Lucien van Wouw <lwo@iisg.nl>
2014-07-10 16:27:54 +0200

    Change file mode for custom files

--- vufind-1.1/import/fop-iish.archieven.sh
+++ vufind-1.1/import/fop-iish.archieven.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-
 # Load all of the .jar files in the lib directory into the classpath
-folder=$VUFIND_HOME/caching/xml
-target=$VUFIND_HOME/caching/ead.pdf
+
+source /usr/local/vufind/custom/config.sh
+
+folder=$VUFIND_HOME/cache/xml
+target=$VUFIND_HOME/cache/pdf
 for file in ${folder}/* ; do
     pdf=$target/$(basename $file .xml).pdf
     echo "Creating $pdf from $file"
     fop -c $VUFIND_HOME/import/fop/fop.xconf -xml $file -xsl $VUFIND_HOME/import/xsl/ead_complete_fo.xsl -pdf $pdf -param path $VUFIND_HOME/import/xsl -param sysYear $(date +'%Y')
     if [ -f "$pdf" ] ; then
-	chown www-data "$pdf"
+	    chown www-data "$pdf"
     fi
 done
